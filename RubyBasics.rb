beaf5ec3b1d7ae15cd7c466ec572be6fbe8918b7
Kevin <walshkev@oregonstate.edu>
2023-10-02 15:21:45 -0700

    ruby basics

---
+++ RubyBasics.rb
@@ -0,0 +1,24 @@
+class Player
+    attr_reader :player_name 
+    attr_reader :player_score
+    def initialize name, score
+        @player_name=name
+        @player_score=score
+    end
+ 
+end
+
+arr = Array.new
+
+for i in 1..50
+    i= Player.new i , rand(0..50)
+    arr << i  
+end
+
+for i in 0..49
+    puts "player name is #{arr[i].player_name} \nplayer score is #{arr[i].player_score}" 
+end
+
+
+
+
